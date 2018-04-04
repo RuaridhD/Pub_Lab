c9bdb9c79374f00b12f0a273c648ac2196e6d988
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-04 11:29:23 +0100

    make all needed files

---
+++ specs/drink_spec.rb
@@ -0,0 +1,10 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../pub.rb")
+require_relative("../customer.rb")
+require_relative("../drink.rb")
+
+class DrinkTest < MiniTest::Test
+
+end 
