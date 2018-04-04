c2287a44692a907c2ecd56e75e53197c4df897a4
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-04 11:54:22 +0100

    make 2 assertions customer.rb

--- customer.rb
+++ customer.rb
@@ -1,1 +1,16 @@
- 
+class Customer
+
+  def initialize(name, wallet)
+    @name = name
+    @wallet = wallet
+  end
+
+  def name()
+    return @name
+  end
+
+  def wallet()
+    return @wallet
+  end
+
+end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -6,5 +6,19 @@ require_relative("../customer.rb")
 require_relative("../drink.rb")
 
 class CustomerTest < MiniTest::Test
- 
+
+def setup()
+  @customer1 = Customer.new("Ruaridh", 50.00)
+  @customer2 = Customer.new("Daniel", 30.00)
+  @customer3 = Customer.new("Connor", 0.00)
+end
+
+def test_name()
+  assert_equal("Connor", @customer3.name)
+end
+
+def test_wallet()
+  assert_equal(30.00, @customer2.wallet)
+end
+
 end
